0c820118b6ad7cda86156c2fa8c2d9d0b1080552
Khalid Husain <khusain444@gmail.com>
2018-09-21 20:03:48 -0400

    implement showing a wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,14 @@
 // ROUTER setup
 const express = require("express");
 const router = express.Router();
-
 //
 const { Page } = require("../models");
-const { addPage } = require("../views/");
+const { addPage, wikiPage, main } = require("../views/");
 
-router.get("/", (req, res, next) => {
-  res.send("GET /wiki");
+router.get("/", async (req, res, next) => {
+  const pages = await Page.findAll()
+//   console.log(pages);  // WRONG AF
+  res.send(main(pages));
 });
 
 router.post("/", async (req, res, next) => {
@@ -23,7 +24,7 @@ router.post("/", async (req, res, next) => {
   // note: `.save` returns a promise.
   try {
     await page.save();
-    res.redirect("/");
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
@@ -35,9 +36,13 @@ router.get("/add", (req, res, next) => {
 });
 
 // For each added wiki-page
-router.get("/:slug", (req, res, next) => {
-  res.send(`hit dynamic route at ${req.params.slug}`);
+router.get("/:slug", async (req, res, next) => {
+//   res.send(`hit dynamic route at ${req.params.slug}`);
   // res.send('GET /wiki/add');  WORKS
+    const pageInfo = await Page.findOne({
+        where: {slug: `${req.params.slug}`}
+    })
+    res.send(wikiPage(pageInfo));
 });
 
 module.exports = router;
